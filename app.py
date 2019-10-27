21223fc08a1b94b6f142c93347045a9dee9216e0
Kevin Hu <kevin.hu@berkeley.edu>
2019-10-26 17:33:56 -0700

    Add google vision

--- app.py
+++ app.py
@@ -44,6 +44,7 @@ server_url = 'https://olock.kevin-hu.org'
 
 # Data filepath
 data_path = 'data.json'
+image_path = 'temp_img.jpg'
 
 # Constants
 success = {'response': 'Success'}
@@ -269,9 +270,31 @@ def data():
 # Image Processing #
 ####################
 
+def saveImage(imgurl):
+    urllib.request.urlretrieve(imgurl, image_path)
+
 # Processes jpg from doorbell and return # people entering
 def process(imgurl):
-    return 0
+    from google.cloud import vision
+    client = vision.ImageAnnotatorClient()
+
+    saveImage(imgurl)
+
+    with open(image_path, 'rb') as image_file:
+        content = image_file.read()
+    image = vision.types.Image(content=content)
+
+    objects = client.object_localization(
+        image=image).localized_object_annotations
+
+    num = len(objects)
+    print('Number of objects found: {}'.format(num))
+    for object_ in objects:
+        print('\n{} (confidence: {})'.format(object_.name, object_.score))
+        print('Normalized bounding polygon vertices: ')
+        for vertex in object_.bounding_poly.normalized_vertices:
+            print(' - ({}, {})'.format(vertex.x, vertex.y))
+    return num
 
 
 ##################
